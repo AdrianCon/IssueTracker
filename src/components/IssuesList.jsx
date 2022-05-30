0372f141fc417156efe2a0e7f89847eafc69bf30
AdrianCon <adrian.dev@tutanota.com>
2022-05-30 15:03:35 -0500

    Add pagination to issues list

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -4,22 +4,25 @@ import {IssueItem} from "./IssueItem";
 import fetchWithError from "../helpers/fetchWithErrors";
 import Loader from "./Loader";
 
-export default function IssuesList({labels, status}) {
+export default function IssuesList({labels, status, pageNum, setPageNum}) {
   const queryClient = useQueryClient();
   const [searchValue, setSearchValue] = useState("");
   const issuesQuery = useQuery(
-    ["issues", {labels, status}],
+    ["issues", {labels, status, pageNum}],
     async ({signal}) => {
       const statusString = status ? `&status=${status}` : '';
       const labelsString = labels.map((label) => `labels[]=${label}`).join('&');
-      const results = await fetchWithError(`api/issues?${labelsString}${statusString}`, {signal});
+      const paginationString = pageNum ? `&page=${pageNum}` : "";
+      
+      const results = await fetchWithError(`api/issues?${labelsString}${statusString}${paginationString}`, {signal});
       
       results.forEach((issue) => {
         queryClient.setQueryData(["issues", issue.number.toString()], issue);
       });
 
       return results;
-    }
+    },
+    {keepPreviousData: true, }
   )
 
   const searchQuery = useQuery(
@@ -56,21 +59,40 @@ export default function IssuesList({labels, status}) {
       {issuesQuery.isLoading ? <p>Loading...</p> :
       issuesQuery.isError ? <p>{issuesQuery.error.message}</p> :
       searchQuery.fetchStatus === "idle" && searchQuery.isLoading ? (
-        <ul className={'issues-list'}>
-          {issuesQuery.data.map((issue) => 
-            <IssueItem
-              key={issue.id}
-              title={issue.title}
-              number={issue.number}
-              assignee={issue.assignee}
-              commentCount={issue.comments.length}
-              createdBy={issue.createdBy}
-              createdDate={issue.createdDate}
-              labels={issue.labels}
-              status={issue.status}
-            />
-          )}
-        </ul>
+        <>
+          <ul className={'issues-list'}>
+            {issuesQuery.data.map((issue) => 
+              <IssueItem
+                key={issue.id}
+                title={issue.title}
+                number={issue.number}
+                assignee={issue.assignee}
+                commentCount={issue.comments.length}
+                createdBy={issue.createdBy}
+                createdDate={issue.createdDate}
+                labels={issue.labels}
+                status={issue.status}
+              />
+            )}
+          </ul>
+          <div className="pagination">
+            <button
+              onClick={() => setPageNum(pageNum-1)}
+              disabled={pageNum === 1}
+            >
+              Previous
+            </button>
+            <p>Page {pageNum} {issuesQuery.isFetching && "..."}</p>
+            <button
+              onClick={() => {
+                if(issuesQuery.data?.length !== 0 && !issuesQuery.isPreviousData) setPageNum(pageNum+1)
+              }}
+              disabled={issuesQuery.data?.length === 0 || issuesQuery.isPreviousData}
+              >
+                Next
+              </button>
+          </div>
+        </>
       ) : (
         <>
           <h2>Search Results</h2>
@@ -79,21 +101,21 @@ export default function IssuesList({labels, status}) {
           ) : (
             <>
               <p>{searchQuery.data.count} Results</p>
-              <ul className="issues-list">
-                {searchQuery.data.items.map((issue) => (
-                  <IssueItem
-                  key={issue.id}
-                  title={issue.title}
-                  number={issue.number}
-                  assignee={issue.assignee}
-                  commentCount={issue.comments.length}
-                  createdBy={issue.createdBy}
-                  createdDate={issue.createdDate}
-                  labels={issue.labels}
-                  status={issue.status}
-                />
-                ))}
-              </ul>
+                <ul className="issues-list">
+                  {searchQuery.data.items.map((issue) => (
+                    <IssueItem
+                    key={issue.id}
+                    title={issue.title}
+                    number={issue.number}
+                    assignee={issue.assignee}
+                    commentCount={issue.comments.length}
+                    createdBy={issue.createdBy}
+                    createdDate={issue.createdDate}
+                    labels={issue.labels}
+                    status={issue.status}
+                  />
+                  ))}
+                </ul>
             </>
           )}
         </>
